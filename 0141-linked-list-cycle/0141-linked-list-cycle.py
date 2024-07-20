844b6aa63ca47ec083c3492cefde35ecb723f353
alcatrazxlr8 <saransh1000@gmail.com>
2024-07-19 20:00:45 -0400

    Time: 43 ms (72.07%), Space: 19.6 MB (10.83%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -7,14 +7,28 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        fast = head
-        
-        while fast and fast.next:
-            fast = fast.next.next
-            head = head.next
-            
-            if head == fast:
+        ### SET
+        visited = set()
+        curr = head
+        while curr:
+            if curr in visited:
                 return True
-        
+            
+            visited.add(curr)
+            curr = curr.next
+            
         return False
+            
+        
+#         ### TORTOISE & HARE [SLOW & FAST POINTER]
+#         fast = head
+        
+#         while fast and fast.next:
+#             fast = fast.next.next
+#             head = head.next
+            
+#             if head == fast:
+#                 return True
+        
+#         return False
             
