8c6f23c3d4d275451358f1004f2236a70a1d1c8e
alcatrazxlr8 <saransh1000@gmail.com>
2024-09-11 11:59:47 -0400

    Time: 242 ms (61.52%), Space: 18.8 MB (95.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,25 @@
 class Solution:
 
-    def dfs(self, grid, i, j, visited):
-        if(i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] == "0" or visited[(i,j)] == 1):
+    def dfs(self, grid, i, j):
+        if(i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] == "0"):
             return
-        visited[(i,j)] = 1
-        self.dfs(grid, i-1, j, visited)
-        self.dfs(grid, i, j-1, visited)
-        self.dfs(grid, i+1, j, visited)
-        self.dfs(grid, i, j+1, visited)
+
+        grid[i][j] = "0"
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j-1)
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i, j+1)
 
     def numIslands(self, grid: List[List[str]]) -> int:
         islands = 0
         m = len(grid)
         n = len(grid[0])
 
-        visited = {}
-        
-        for i in range(m):
-            for j in range(n):
-                visited[(i,j)] = 0
                 
         for i in range(m):
             for j in range(n):
-                if(grid[i][j] == "1" and visited[(i,j)] != 1):
-                    self.dfs(grid, i, j, visited)
+                if(grid[i][j] == "1"):
+                    self.dfs(grid, i, j)
                     islands += 1
-                # visited[i][j] = 1
             
         return islands
