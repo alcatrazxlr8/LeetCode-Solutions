216e6ab59ddb11f76264155ab1282f427c1b069a
alcatrazxlr8 <saransh1000@gmail.com>
2024-10-12 23:38:33 -0400

    Time: 35 ms (87.03%), Space: 17 MB (10.91%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,32 @@
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        
+    def permute(self, nums: List[int]) -> List[List[int]]:  
         
         n = len(nums)
         ans = []
-        freq = [0] * n
+#         freq = [0] * n
         
-        def rec(freq, arr):
-            if len(arr) >= n:
-                ans.append(arr.copy())
+#         def rec(freq, arr):
+#             if len(arr) >= n:
+#                 ans.append(arr.copy())
+#                 return
+#             for i in range(n):
+#                 if not freq[i]:
+#                     arr.append(nums[i])
+#                     freq[i] = 1
+#                     rec(freq, arr)
+#                     freq[i] = 0
+#                     arr.pop()
+#         rec(freq, [])            
+
+        def rec(i):
+            if i >= n:
+                ans.append(nums.copy())
+                # print(ans)
                 return
-            for i in range(n):
-                if not freq[i]:
-                    arr.append(nums[i])
-                    freq[i] = 1
-                    rec(freq, arr)
-                    freq[i] = 0
-                    arr.pop()
-        rec(freq, [])            
+            for j in range(i, n):
+                nums[i], nums[j] = nums[j], nums[i]
+                rec(i+1)
+                nums[i], nums[j] = nums[j], nums[i]
+        rec(0)        
         return ans
             
