1558cb727388378eca0420736fcd720f1d301c68
alcatrazxlr8 <saransh1000@gmail.com>
2025-01-16 13:24:05 -0500

    Time: 34 ms (44.85%), Space: 55.1 MB (45.03%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -3,11 +3,13 @@ class Solution:
         a, b, c = target[0], target[1], target[2]
         maxA, maxB, maxC = 0, 0, 0
         for i in range(len(triplets)):
-            if (a < triplets[i][0] or b < triplets[i][1] or c < triplets[i][2]) or (a != triplets[i][0] and b != triplets[i][1] and c != triplets[i][2]):
+            # we cant go over target values(1st condition) & we dont need triplets that have none of the target values (2nd condition)
+            if (triplets[i][0] > a or triplets[i][1] > b or triplets[i][2] > c) or (a != triplets[i][0] and b != triplets[i][1] and c != triplets[i][2]):
                 continue
             else:
                 maxA, maxB, maxC = max(maxA, triplets[i][0]), max(maxB, triplets[i][1]), max(maxC, triplets[i][2])
 
-        return True if (maxA, maxB, maxC) == (a, b, c) else False
+        return (maxA, maxB, maxC) == (a, b, c)
+        # return True if (maxA, maxB, maxC) == (a, b, c) else False
                 
 
