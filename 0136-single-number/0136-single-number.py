0b9b2c2b3ecdcbe4991694269e2fb7a61a2ad960
alcatrazxlr8 <saransh1000@gmail.com>
2025-02-11 15:05:05 -0600

    Time: 0 ms (100%), Space: 19.5 MB (89.06%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,5 +2,8 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
         for num in nums:
+            # print(f"result = {result} ; num = {num} ; xor = {result ^ num}")
+            # I had trouble understanding why the subsequent occurances cancelled out, 
+            # but looking at the binary representation of nums and result helped clarify things
             result ^= num
         return result
