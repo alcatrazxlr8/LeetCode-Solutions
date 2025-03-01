d0613317df8f0bffa8ca5533d3656f40887ecc02
alcatrazxlr8 <saransh1000@gmail.com>
2025-02-28 19:38:11 -0600

    Time: 19 ms (59.84%), Space: 18.1 MB (34.69%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -2,7 +2,10 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         big = Counter(magazine)
         small = Counter(ransomNote)
-        for key, value in small.items():
-            if big[key] < value or not big[key]:
-                return False
-        return True
+
+        return big & small == small
+
+        # for key, value in small.items():
+        #     if big[key] < value or not big[key]:
+        #         return False
+        # return True
