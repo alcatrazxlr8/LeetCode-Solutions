bfa6b1904354efe1467581144a01f65dd2767660
alcatrazxlr8 <saransh1000@gmail.com>
2025-03-05 13:17:23 -0600

    Time: 0 ms (100%), Space: 17.7 MB (48.12%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,7 +1,15 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        ans = [0]*n
-        ans[0] = 1
-        for i in range(1, n):
-            ans[i] = ans[i-1] + 4*i
-        return ans[n-1]
+        
+        # 1 + 4(1) + 4(2) + 4(3) + 4(4) + .....
+        # 1 + 4 (1 + 2 + 3 + 4 + 5 + .....)
+        # 1 + 4(n*(n-1)/2)
+        return 1 + 2*n*(n-1)
+
+
+        
+        # ans = [0]*n
+        # ans[0] = 1
+        # for i in range(1, n):
+        #     ans[i] = ans[i-1] + 4*i
+        # return ans[n-1]
