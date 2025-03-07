2d10ebfcc06198898c722803a48640b347b2c4a6
alcatrazxlr8 <saransh1000@gmail.com>
2025-03-07 15:03:02 -0600

    Time: 635 ms (35.15%), Space: 20.3 MB (89.83%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        ans = set()
         nums.sort()
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
@@ -11,12 +11,14 @@ class Solution:
             
             while left < right:
                 if nums[left] + nums[right] == target:
-                    ans.append([nums[i], nums[left], nums[right]])
+                    trip = [nums[i], nums[left], nums[right]]
+                    trip = tuple(trip)
+                    ans.add(trip)
                     left += 1
-                    while nums[left] == nums[left-1] and left < right:
-                        left +=1
+                    # while nums[left] == nums[left-1] and left < right:
+                    #     left +=1
                 elif nums[left] + nums[right] < target:
                     left += 1
                 else:
                     right -= 1
-        return ans
+        return list(ans)
