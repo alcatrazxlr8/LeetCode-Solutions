b98cebe9ada4f5e488d389b3d9a0b2063d07cb24
alcatrazxlr8 <saransh1000@gmail.com>
2025-03-07 14:06:50 -0600

    Time: 6 ms (30.23%), Space: 18.5 MB (59.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -2,16 +2,11 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left = 0
         right = len(numbers) - 1
-        hashmap = defaultdict(int)
-        # ans = []
-        for i in range(len(numbers)):
-            complement = target - numbers[i]
-            if complement in hashmap:
-                return [min(i+1, hashmap[complement]+1), max(i+1, hashmap[complement]+1)]
-            hashmap[numbers[i]] = i
-                # ans.append(hashmap[complement]+1)
-                # ans.append(i+1)
-            # else:
-                # hashmap[numbers[i]] = i
-        # return ans
-                
+        while left < right:
+            total = numbers[left] + numbers[right]
+            if total == target:
+                return [left+1, right+1]
+            if total < target:
+                left += 1
+            else:
+                right -= 1
