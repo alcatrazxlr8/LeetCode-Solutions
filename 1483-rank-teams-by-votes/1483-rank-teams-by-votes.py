abbbb39e977e7359dc8f6e22e0983b3355128258
alcatrazxlr8 <saransh1000@gmail.com>
2025-03-18 14:17:44 -0500

    Time: 12 ms (78.32%), Space: 18.1 MB (78.57%) - LeetHub

---
+++ 1483-rank-teams-by-votes/1483-rank-teams-by-votes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+            teams = list(votes[0])
+            numTeams = len(teams)
+            # ranks = [[0 for _ in range(numTeams)] for _ in range(numTeams)] 
+            ranks = {team: [0]* numTeams for team in votes[0]}
+            
+            for vote in votes:
+                for rank, team in enumerate(vote):
+                    ranks[team][rank] -= 1
+            sortedTeams = sorted(votes[0], key=lambda team:(ranks[team], team))
+            return "".join(sortedTeams)
