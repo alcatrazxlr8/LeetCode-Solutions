9e70638e9b85df8b86537b798d7f8e9a161588ab
alcatrazxlr8 <saransh1000@gmail.com>
2025-03-18 21:49:01 -0500

    Time: 230 ms (91.81%), Space: 20.1 MB (82.9%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,40 @@
 class Solution:
-
-    def dfs(self, grid, i, j):
-        if(i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] == "0"):
-            return
-
-        grid[i][j] = "0"
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i, j-1)
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i, j+1)
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
-        m = len(grid)
-        n = len(grid[0])
+        if not grid:
+            return 0
 
-                
-        for i in range(m):
-            for j in range(n):
-                if(grid[i][j] == "1"):
-                    self.dfs(grid, i, j)
-                    islands += 1
+        def dfs(row, col):
+            if (row >= rows or col >= cols or row < 0 or col < 0 or grid[row][col] == "0"):
+                return
+
+            grid[row][col] = "0"
+            dfs(row+1, col)
+            dfs(row, col+1)
+            dfs(row-1, col)
+            dfs(row, col-1)
             
+
+        def bfs(row, col):
+            q = deque()
+            q.append((row, col))
+            grid[row][col] = "0"
+            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if ( rows > nr >= 0 and cols > nc >= 0 and grid[nr][nc] == "1"):
+                        q.append((nr, nc))
+                        grid[nr][nc] = "0"
+
+        rows = len(grid)
+        cols = len(grid[0])
+        islands = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    dfs(i, j)
+                    # bfs(i, j)
+                    islands += 1
         return islands
