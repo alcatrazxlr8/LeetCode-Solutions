9e86d847665b0ec87f4e8cf4be795d6404a24379
alcatrazxlr8 <saransh1000@gmail.com>
2025-04-30 00:37:20 -0500

    Time: 0 ms (100%), Space: 18.2 MB (72.2%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,24 +1,28 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        left = 0
-        right = (rows * cols) - 1
-        
-        while(left <= right):
-            mid = left + int((right - left)/2)
-            # midRow = math.ceil(mid/cols) - 1
-            midRow = mid // cols
-            midCol = mid % cols
-            
-            
-            # print(matrix[midRow][midCol])
-            if matrix[midRow][midCol] == target:
-                return True
-            elif matrix[midRow][midCol] > target:
-                right = mid - 1
-            elif matrix[midRow][midCol] < target:
-                left = mid + 1
-                
+        m = len(matrix)
+        n = len(matrix[0])
+
+        low_row = 0
+        high_row = m - 1
+        while low_row <= high_row:
+            mid_row = low_row + (high_row - low_row)//2
+            if matrix[mid_row][0] <= target <= matrix[mid_row][n-1]:
+                ## binary search in this row again
+                l = 0
+                r = n-1
+                while l <= r:
+                    mid = l + (r-l)//2
+                    if matrix[mid_row][mid] == target:
+                        return True
+                    elif matrix[mid_row][mid] < target:
+                        l = mid + 1
+                    else:
+                        r = mid - 1
+                return False
+
+            elif target < matrix[mid_row][0]:
+                high_row = mid_row - 1
+            elif target > matrix[mid_row][n-1]:
+                low_row = mid_row + 1
         return False
-            
