5fe6087c26a8e066a24384c1a944a28d8f1498a6
alcatrazxlr8 <saransh1000@gmail.com>
2025-05-03 02:23:52 -0500

    Time: 0 ms (100%), Space: 23.4 MB (38.93%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,26 +8,40 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        #find mid
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        #slow.next is start of 2nd half
-        
-        #reverse 2nd half
-        second = slow.next
-        prev = slow.next = None
-        while second:
-            nxt = second.next
-            second.next = prev
-            prev = second
-            second = nxt
-        
-        #merge
-        first, second = head, prev #prev is the head of the reversed second list
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
+        if not head.next:
+            return
+
+        # find center
+        def findCenter(head) -> ListNode:
+            slow, fast = head, head.next
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            center = slow.next
+            slow.next = None
+            return center
+
+        # reverse 2nd half - from center to end
+        def reverse(head):
+            center = findCenter(head)
+            prev = None
+            curr = center
+            while curr:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            tail = prev
+            return tail
+
+        start, tail = head, reverse(head)
+
+        while tail:
+            startNext = start.next
+            tailNext = tail.next
+            start.next = tail
+            tail.next = startNext
+            start = startNext
+            tail = tailNext
+
+
