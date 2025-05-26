9006d16015aadc8f280b53d374a9ee5f2ef08354
alcatrazxlr8 <saransh1000@gmail.com>
2025-05-26 18:52:28 -0400

    Time: 1 ms (99.63%), Space: 18.1 MB (39.01%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -5,47 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        if (p and not q) or (not p and q):
-            return False
-        if p and q:
-            if (p.val != q.val):
-                return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-    
-    
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if (not root and not subRoot) or (root and not subRoot):
-            return True
-        if (not root and subRoot):
-            return False
-        if (root and subRoot):
-            if (root.val != subRoot.val):
-                    return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-            else:
-                if self.isSameTree(root, subRoot):
-                    return self.isSameTree(root, subRoot)
-                else:
-                    return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) 
-        
-        return False
-                
-                
-            
-        
-        
-        
-        
-        
-        # if (not root and not subRoot):
-        #     return True
-        # if (not root and subRoot) or (root and not subRoot):
-        #     return False
-        # # if root and subRoot:
-        # # if (root.val == subRoot.val):
-        # else:
-        #     return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        # return True
+        def serialized(root):
+            if not root:
+                return "#"
+            return f"({root.val},{serialized(root.left)},{serialized(root.right)})"
+        rootSer = serialized(root)
+        subrootSer = serialized(subRoot)
+        return subrootSer in rootSer
