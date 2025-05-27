d4b8fc5b9e59ca55e03dbee41e912cd12130ba78
alcatrazxlr8 <saransh1000@gmail.com>
2025-05-27 19:02:08 -0400

    Time: 3 ms (37.12%), Space: 21.1 MB (72.09%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,14 +6,29 @@
 #         self.right = right
 class Solution:
     
-    def inorder(self, root, res):
-        if not root:
-            return
-        self.inorder(root.left, res)
-        res.append(root.val)
-        self.inorder(root.right, res)
+    # def inorder(self, root, res):
+    #     if not root:
+    #         return
+    #     self.inorder(root.left, res)
+    #     res.append(root.val)
+    #     self.inorder(root.right, res)
     
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        res = []
-        self.inorder(root, res)
-        return res[k-1]
+    # def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    #     res = []
+    #     self.inorder(root, res)
+    #     return res[k-1]
+
+        def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:         
+            res = []
+            def dfs(root):
+                if not root:
+                    return
+                dfs(root.left)
+                heapq.heappush(res, root.val)
+                dfs(root.right)
+            
+            dfs(root)
+            while k > 0:
+                tmp = heapq.heappop(res)
+                k -= 1
+            return tmp
