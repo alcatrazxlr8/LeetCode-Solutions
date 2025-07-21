66156529456ec38b779dc77464aa9d7b060fab77
alcatrazxlr8 <saransh1000@gmail.com>
2025-07-20 23:42:55 -0400

    Time: 43 ms (84.81%), Space: 34.4 MB (72.9%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,27 +2,25 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         
         res = []
-        path = []
-        
-        def isPalindrome(s, start, end):
-            while(start <= end):
-                if s[start] != s[end]:
+        part = []
+
+        def isPali(s, l, r):
+            while l < r:
+                if s[l] != s[r]:
                     return False
-                start += 1
-                end -= 1
+                l += 1
+                r -= 1
             return True
-            
-        def rec(s, index):
-            if index == len(s):
-                res.append(path.copy())
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
                 return
             
-            for i in range(index, len(s)):
-                if isPalindrome(s, index, i):
-                    path.append(s[index : i + 1])
-                    rec(s, i + 1)
-                    path.pop()
-                    
-        rec(s, 0)
+            for j in range(i, len(s)):
+                if isPali(s, i, j):
+                    part.append(s[i:j+1])
+                    dfs(j+1)
+                    part.pop()
+        dfs(0)
         return res
-            
