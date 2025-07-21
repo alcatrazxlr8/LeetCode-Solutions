0be4d228125b9dc1b348f75e0e5d35db62df7416
alcatrazxlr8 <saransh1000@gmail.com>
2025-07-20 23:53:43 -0400

    Time: 0 ms (100%), Space: 18 MB (22.93%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,18 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        ## 
         numLetterMap = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
-        
-        n = len(digits)
-        ans = []
-        
-        def rec(i, tmp):
-            if len(tmp) == n:
-                ans.append(tmp)
+
+        res = []
+
+        def dfs(i, curr):
+            if len(curr) == len(digits):
+                res.append(curr)
                 return
             for c in numLetterMap[digits[i]]:
-                rec(i+1, tmp + c)
-                
+                dfs(i+1, curr + c)
+
         if digits:
-            rec(0, "")
-        return ans
+            dfs(0, "")
+            
+        return res
